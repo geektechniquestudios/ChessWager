0f48e006b5331b628e31336de9296e476b79d0f8
geektechniquestudios <geektechniquestudios@gmail.com>
2021-10-02 19:26:20 -0400

    test commit

--- src/components/footer/MainFooter.tsx
+++ src/components/footer/MainFooter.tsx
@@ -3,6 +3,8 @@ const MainFooter = () => {
         <div id="main-footer">
             {/* facebook, youtube, twitter, instagram, ?reddit, contact
             evenly spaced flexbox probably
+
+            
             */}
 
             <a href="https://www.google.com/"><img src="some/file/path" alt=""/></a>
