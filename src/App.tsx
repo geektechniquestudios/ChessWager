9dd179129d5d8303aa3a68d69d0b4b70147ac006
geektechniquestudios <geektechniquestudios@gmail.com>
2021-11-08 02:37:20 -0500

    buttons styled, user imgs styled, dark mode storage drafted

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import BettingLobby from "./components/lobby/BettingLobby"
 import ChessGame from "./components/game/ChessGame"
@@ -7,9 +7,43 @@ import GlobalChat from "./components/chat/GlobalChat"
 import Footer from "./components/footer/MainFooter"
 import "./style/index.css"
 import "./config"
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
+import firebase from "firebase/compat"
+import { Auth } from "./components/containers/Auth"
 
 const App: React.FC = () => {
-  const [isDarkOn, setIsDarkOn] = useState(false)
+  //state for if dark is on
+
+  const { auth, user } = Auth.useContainer()
+
+  useEffect(() => {
+    if (auth.currentUser?.uid) {
+      const userRef: firebase.firestore.DocumentReference = firebase
+        .firestore()
+        .collection("users")
+        .doc(auth.currentUser!.uid)
+
+      userRef
+        .get()
+        .then(doc => doc.data()?.darkMode)
+        .then(darkMode => {
+          setIsDarkOn(darkMode)
+          console.log(darkMode)
+        })
+        .catch(console.error)
+    } else {
+      // const darkMode = localStorage.getItem("darkMode")
+      // if (darkMode) {
+      //   setIsDarkOn(darkMode === "true")
+      // }
+    }
+
+
+  }, [])
+
+    const [isDarkOn, setIsDarkOn] = useState(true)
+
 
   return (
     <div className={isDarkOn ? "dark" : ""}>
@@ -22,7 +56,7 @@ const App: React.FC = () => {
         min-w-full
         "
         >
-          <MainHeader setIsDarkOn={setIsDarkOn} />
+          <MainHeader isDarkOn={isDarkOn} setIsDarkOn={setIsDarkOn}/>
         </header>
         <nav>
           <BettingLobby />
