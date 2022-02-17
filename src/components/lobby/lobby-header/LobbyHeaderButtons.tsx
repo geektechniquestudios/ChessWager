486d1885c29b9f811c81137ffaa3be1c28430b1b
geektechniquestudios <geektechniquestudios@gmail.com>
2022-02-17 16:37:53 -0500

    lobby header reorder

--- src/components/lobby/lobby-header/LobbyHeaderButtons.tsx
+++ src/components/lobby/lobby-header/LobbyHeaderButtons.tsx
@@ -6,10 +6,10 @@ import { HeaderButton } from "./LobbyHeaderButton"
 
 export const LobbyHeaderButtons: React.FC = () => {
   return (
-    <div className="flex justify-between w-full px-3">
+    <div className="flex justify-between w-full px-20">
       <HeaderButton buttonName="Trust" buttonIcon={<FaRegHandshake />} />
-      <HeaderButton buttonName="Side" buttonIcon={<GiYinYang />} />
       <HeaderButton buttonName="Amount" buttonIcon={<BsPiggyBank />} />
+      <HeaderButton buttonName="Side" buttonIcon={<GiYinYang />} />
       <HeaderButton buttonName="Age" buttonIcon={<FaRegClock />} />
       <HeaderButton
         buttonName="Multiplier"
