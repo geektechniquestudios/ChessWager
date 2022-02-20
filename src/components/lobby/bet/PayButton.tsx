e5859ab9f5e8b25bc50ce62ee04ffbbfdf7fe5b6
geektechniquestudios <geektechniquestudios@gmail.com>
2022-02-19 22:03:10 -0500

    ascdesc hover color change, pay buton changed to wallet

--- src/components/lobby/bet/PayButton.tsx
+++ src/components/lobby/bet/PayButton.tsx
@@ -1,4 +1,4 @@
-import { BiDollar } from "react-icons/bi"
+import { BiDollar, BiWallet } from "react-icons/bi"
 
 export const PayButton: React.FC<any> = (sendBet: () => void) => {
   return (
@@ -7,7 +7,7 @@ export const PayButton: React.FC<any> = (sendBet: () => void) => {
         className="color-shift w-8 h-8 grid place-content-center hover:bg-stone-300 dark:hover:bg-stone-800 rounded-md animate-pulse"
         onClick={sendBet}
       >
-        <BiDollar size="24" title="Send Wager" color="green" />
+        <BiWallet size="24" title="Send Wager" color="green" />
       </button>
     </div>
   )
