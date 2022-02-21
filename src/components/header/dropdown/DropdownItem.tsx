a9ef5cd3d6b9117fec69231c63ae8772bd402082
geektechniquestudios <geektechniquestudios@gmail.com>
2022-02-20 20:44:51 -0500

    dropdown items unrounded

--- src/components/header/dropdown/DropdownItem.tsx
+++ src/components/header/dropdown/DropdownItem.tsx
@@ -23,7 +23,7 @@ export const DropdownItem: React.FC<Props> = ({
     // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <a
       href={address}
-      className="h-14 w-64 px-4 flex items-center rounded-md hover:bg-stone-300 dark:hover:bg-stone-600 dark:text-stone-200 dark:hover:text-stone-200 color-shift"
+      className="h-14 w-64 px-4 flex items-center hover:bg-stone-300 dark:hover:bg-stone-600 dark:text-stone-200 dark:hover:text-stone-200 color-shift"
       onClick={() => {
         onClick && onClick()
         goToMenu && setActiveMenu(goToMenu)
