0c3a039fcb4e1f131f39e2d7ff742bc186748d62
spaceC00kie <60335398+spaceC00kie@users.noreply.github.com>
2022-02-22 19:37:11 -0500

    fragment removed

--- src/components/header/dropdown/menus/Profile.tsx
+++ src/components/header/dropdown/menus/Profile.tsx
@@ -22,64 +22,62 @@ export const Profile: React.FC = () => {
   }
 
   return (
-    <>
-      <Menu
-        menuItems={[
-          <DropdownItem
-            goToMenu="main"
-            leftIcon={<BiArrowBack />}
-            key={0}
-            text="Profile"
-          />,
-          <div
-            className="border-b-2 border-stone-400 dark:border-stone-600"
-            key={1}
-          />,
-          <DropdownItem
-            goToMenu="notifications"
-            leftIcon={<RiNotification3Line />}
-            key={2}
-            text="Notifications"
-          />,
-          <DropdownItem
-            goToMenu="friends"
-            leftIcon={<FiUsers />}
-            key={3}
-            text="Friends"
-          />,
-          <DropdownItem
-            goToMenu="messages"
-            leftIcon={<RiChat2Line />}
-            key={4}
-            text="Messages"
-          />,
-          <DropdownItem leftIcon={<BsGraphUp />} key={5} text="Stats" />,
-          <DropdownItem leftIcon={<FiAward />} key={6} text="Achievements" />,
-          <DropdownItem leftIcon={<FiUser />} key={7} text="Display Name" />,
-          <DropdownItem
-            leftIcon={
-              <button
-                onClick={() => {
-                  // uploadImage()
-                }}
-              >
-                <AiOutlineCloudUpload />
-              </button>
-            }
-            onClick={() => {}}
-            rightIcon={
-              <img
-                src={user!.photoURL!}
-                alt=""
-                className="w-6 h-6 rounded-full mr-2"
-              />
-            }
-            key={8}
-            text="User Image"
-          />,
-        ]}
-        thisMenu={"profile"}
-      />
-    </>
+    <Menu
+      menuItems={[
+        <DropdownItem
+          goToMenu="main"
+          leftIcon={<BiArrowBack />}
+          key={0}
+          text="Profile"
+        />,
+        <div
+          className="border-b-2 border-stone-400 dark:border-stone-600"
+          key={1}
+        />,
+        <DropdownItem
+          goToMenu="notifications"
+          leftIcon={<RiNotification3Line />}
+          key={2}
+          text="Notifications"
+        />,
+        <DropdownItem
+          goToMenu="friends"
+          leftIcon={<FiUsers />}
+          key={3}
+          text="Friends"
+        />,
+        <DropdownItem
+          goToMenu="messages"
+          leftIcon={<RiChat2Line />}
+          key={4}
+          text="Messages"
+        />,
+        <DropdownItem leftIcon={<BsGraphUp />} key={5} text="Stats" />,
+        <DropdownItem leftIcon={<FiAward />} key={6} text="Achievements" />,
+        <DropdownItem leftIcon={<FiUser />} key={7} text="Display Name" />,
+        <DropdownItem
+          leftIcon={
+            <button
+              onClick={() => {
+                // uploadImage()
+              }}
+            >
+              <AiOutlineCloudUpload />
+            </button>
+          }
+          onClick={() => {}}
+          rightIcon={
+            <img
+              src={user!.photoURL!}
+              alt=""
+              className="w-6 h-6 rounded-full mr-2"
+            />
+          }
+          key={8}
+          text="User Image"
+        />,
+      ]}
+      thisMenu={"profile"}
+    />
   )
 }
