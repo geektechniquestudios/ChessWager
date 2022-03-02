21b133e73d76b5ca98193220b498880ea11ebf85
spaceC00kie <60335398+spaceC00kie@users.noreply.github.com>
2022-03-02 01:27:30 -0500

    textarea added to search area

--- src/components/header/dropdown/areas/SearchArea.tsx
+++ src/components/header/dropdown/areas/SearchArea.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
+
 interface Props {}
 
+const [formValue, setFormValue] = useState("")
+
 export const SearchArea: React.FC<Props> = ({}) => {
-  return <p>Sample for SpaceC00kie</p>
+  return (
+    <textarea
+      value={formValue}
+      onChange={(e) => setFormValue(e.target.value)}
+      className="input break-words inline-block resize-none outline-none text-lg w-full p-2 bg-stone-200 dark:bg-stone-800 dark:text-stone-50"
+      placeholder="Search Users"
+      onKeyDown={(e) => {}}
+    />
+  )
 }
