21debeb6fd5baa8d05a18125295e650321290033
spaceC00kie <60335398+spaceC00kie@users.noreply.github.com>
2022-03-11 20:01:45 -0500

    added friends list

--- src/components/header/dropdown/menus/Friends.tsx
+++ src/components/header/dropdown/menus/Friends.tsx
@@ -1,5 +1,7 @@
 import { BiArrowBack } from "react-icons/bi"
+import { DropdownArea } from "../DropdownArea"
 import { DropdownItem } from "../DropdownItem"
+import { FriendsList } from "./FriendsList"
 import { Menu } from "../Menu"
 
 export const Friends: React.FC = ({}) => {
@@ -14,6 +16,10 @@ export const Friends: React.FC = ({}) => {
             text="Friends"
           />,
           <div className="border-b-2" key={1} />,
+          <DropdownArea
+          key={2}
+          content={<FriendsList/>}
+          />,
         ]}
         thisMenu={"friends"}
       />
