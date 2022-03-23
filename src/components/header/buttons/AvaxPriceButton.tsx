3a645de6639ed0ef40a7f388b2b57244aaf11685
geektechnique <geektechniquestudios@gmail.com>
2022-03-22 21:32:26 -0400

    fixed price button tooltip

--- src/components/header/buttons/AvaxPriceButton.tsx
+++ src/components/header/buttons/AvaxPriceButton.tsx
@@ -14,12 +14,14 @@ export const AvaxPriceButton: React.FC = () => {
         <a
           href="https://www.coingecko.com/en/coins/avalanche"
           className="cw-button flex flex-col justify-center border-none my-2 hover:bg-stone-300 dark:hover:bg-stone-700 h-9"
+          title="Price from CoinGecko"
+          data-toggle="tooltip"
+          target="_blank"
+          rel="noreferrer"
         >
           <p
             style={{ textDecoration: "underline solid #9f1239 3px" }}
             className="m-1 underline"
-            title="Price from CoinGecko"
-            data-toggle="tooltip"
           >
             {`AVAX $${price
               ?.toFixed(2)
