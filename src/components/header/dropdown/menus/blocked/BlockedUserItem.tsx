44d021dc2f6d7d923a9d01a5d4aee6726b6cdc69
geektechnique <geektechniquestudios@gmail.com>
2022-04-11 18:20:43 -0400

    unblocking styled

--- src/components/header/dropdown/menus/blocked/BlockedUserItem.tsx
+++ src/components/header/dropdown/menus/blocked/BlockedUserItem.tsx
@@ -1,4 +1,5 @@
 import { collection, deleteDoc, doc, getFirestore } from "firebase/firestore"
+import { CgUnblock } from "react-icons/cg"
 import { firebaseApp } from "../../../../../config"
 import { Auth } from "../../../../containers/Auth"
 
@@ -34,8 +35,12 @@ export const BlockedUserItem: React.FC<Props> = ({
         <p className="flex flex-col justify-center">{userName}</p>
       </div>
       <div className="flex flex-col justify-center">
-        <button onClick={unBlockUser} className="border rounded-md p-2">
-          unblock
+        <button
+          onClick={unBlockUser}
+          className="w-9 h-9 rounded-md grid place-content-center color-shift clickable border hover:bg-stone-100 dark:hover:bg-stone-700 hover:text-black hover:border-black dark:hover:text-white dark:hover:border-white border-stone-800 dark:border-stone-300 text-stone-800 dark:text-stone-300"
+          title="Unblock"
+        >
+          <CgUnblock className="text-green-600" />
         </button>
       </div>
     </div>
