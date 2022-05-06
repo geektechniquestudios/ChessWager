eb71dd3cf2555d482fd25a6b5db4ffc91020a080
spaceC00kie <kirstie317@gmail.com>
2022-05-06 16:12:08 -0400

    lichess button test

--- cypress/integration/header_left.spec.tsx
+++ cypress/integration/header_left.spec.tsx
@@ -1,5 +1,15 @@
+beforeEach(() => {
+  cy.visit("/")
+})
+
 describe("lichess button", () => {
-  it("should open to the lichess tv link in a new tab", () => {})
+  it("should open to the lichess tv link in a new tab", () => {
+    cy.get('a[id="lichess-button"]').should("have.attr", "target", "_blank")
+    cy.get('a[id="lichess-button"]').then((res) => {
+      const url = res.prop("href")
+      cy.request(url)
+    })
+  })
 })
 
 describe("avax price button", () => {
--- src/components/header/buttons/LichessButton.tsx
+++ src/components/header/buttons/LichessButton.tsx
@@ -5,6 +5,7 @@ interface Props {}
 export const LichessButton: React.FC<Props> = ({}) => {
   return (
     <a
+      id="lichess-button"
       href={`https://lichess.org/tv`}
       className="w-9 h-9 rounded-md grid place-content-center color-shift clickable border-none hover:bg-stone-300 dark:hover:bg-stone-700 hover:text-black hover:border-black dark:hover:text-white dark:hover:border-white border-stone-800 dark:border-stone-300 text-stone-800 dark:text-stone-300"
       title="Watch on Lichess"
