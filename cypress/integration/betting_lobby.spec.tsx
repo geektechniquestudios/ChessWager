c2c6551d3ef7cd88721a595a977c43503163c729
geektechniquestudios <geektechniquestudios@gmail.com>
2022-05-12 06:19:10 -0400

    betting lobby intent more drafted

--- cypress/integration/betting_lobby.spec.tsx
+++ cypress/integration/betting_lobby.spec.tsx
@@ -3,20 +3,19 @@
 
 beforeEach(() => {
   cy.visit("/")
+  cy.login()
 })
+afterEach(cy.logout)
 
 describe("betting lobby", () => {
   it("should allow logged in users to click on a bet, making it selected", () => {
-    cy.login()
     // create bet with call firestore
     // get bet body, look for new bet
     // check state of bet "isSelected"
     // click on bet
     // check state of bet "isSelected"
-    cy.logout()
   })
   it("shouldn't allow unauthenticated users to click bets", () => {
-    cy.login()
     // create bet with call firestore
     cy.logout()
     cy.login("XGXaJZxzR9gArv6wKEHZ5MuvSnd2")
@@ -24,34 +23,56 @@ describe("betting lobby", () => {
     // check state of bet "isSelected"
     // click on bet
     // check state of bet "isSelected"
-    cy.logout()
   })
   it("should keep selected bets in place as other bets move around", () => {
-    cy.login()
     // create 3 bets with call firestore
     cy.logout()
     cy.login("XGXaJZxzR9gArv6wKEHZ5MuvSnd2")
     // get bet body, get middle bet, click on it
     // reorder bets with sorting button "created at"
     // check that middle bet has same "created at" as before
-    cy.logout()
   })
   it("should allow users to join", () => {
-    cy.login()
     // create bet with call firestore
     // get bet body, look for new bet
-    // check for join button within 
+    // check for join button within
     // click join button
   })
   it("should allow bet creator to delete own bet", () => {
-    
+    // create bet with call firestore
+    // test delete button
+    // check that bet is gone
   })
-  // it("should allow bet creator to kick other user", () => {})
-  // it("should allow bet creator to accept other user", () => {})
-  // it("should open the persona menu if a user title is clicked", () => {})
-  // it("should update user stats based on outcome", () => {})
-  // it("should correctly sort bets based on each lobby header button", () => {})
-  // it("should despawn funded bets", () => {})
-  // it("should despawn deleted bets", () => {})
+  it("should allow bet creator to kick other user", () => {
+    // create bet with call firestore in pending state with both users
+    // test delete button
+    // check that bet is gone
+  })
+  it("should allow bet creator to accept other user", () => {
+    // create bet with call firestore in pending state with both users
+    // test approve button
+    // check that bet is gone
+  })
+  it("should open the persona menu if a user title is clicked", () => {
+    // create bet with call firestore in pending state with both users
+    // find user title, click on it
+    // check that clickedUser menu is open
+  })
+  // needs to be part of e2e tests
+  // it("should update user stats based on outcome", () => {
+  //   //
+  // })
+  it("should correctly sort bets based on each lobby header button", () => {
+    // create 5 bets with call firestore
+    // make variety  of qualities and their expected orders
+  })
+  it("should despawn funded bets and spawn bet in funded area", () => {
+    // create bet with call firestore in approved state
+    // check that bet is in lobby
+    // check that bet is not in funded area
+    // update bet to funded state
+    // check that bet is not in lobby
+    // check that bet is in funded area
+  })
   // it("should allow user to pay", () => {})
 })
