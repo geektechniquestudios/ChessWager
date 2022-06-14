931230a9bdef31bd46193f2974fda3f4502f4e31
geektechniquestudios <geektechniquestudios@gmail.com>
2022-06-14 01:42:11 -0400

    cypress upgrade from 9 to 10

---
+++ cypress.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  env: {
+    "cypress-react-selector": {
+      root: "#root",
+    },
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.ts")(on, config)
+    },
+    specPattern: "cypress/e2e/**/*.spec.tsx",
+    baseUrl: "http://localhost:3000",
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    setupNodeEvents(on, config) {},
+    specPattern: "src/**/*.spec.tsx",
+  },
+})
