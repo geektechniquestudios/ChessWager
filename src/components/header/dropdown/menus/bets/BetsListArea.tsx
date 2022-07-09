1ffb399b1a502c8b4d629f4895475953acb05571
geektechniquestudios <geektechniquestudios@gmail.com>
2022-07-09 00:27:26 -0400

    dark mode typings improved, bet list inf scroll rough draft

--- src/components/header/dropdown/menus/bets/BetsListArea.tsx
+++ src/components/header/dropdown/menus/bets/BetsListArea.tsx
@@ -1,10 +1,13 @@
 import "../../../../../style/scrollbar.scss"
 import {
   collection,
+  getDocs,
   getFirestore,
   limit,
+  orderBy,
   Query,
   query,
+  startAfter,
   where,
 } from "firebase/firestore"
 import { firebaseApp } from "../../../../../config"
@@ -12,6 +15,10 @@ import { Auth } from "../../../../containers/Auth"
 import { useCollectionData } from "react-firebase-hooks/firestore"
 import { Bet } from "../../../../../interfaces/Bet"
 import { BetsListItem } from "./BetsListItem"
+import { useState } from "react"
+import InfiniteScroll from "react-infinite-scroll-component"
+import { ThemeProvider } from "@mui/system"
+import { LinearProgress } from "@mui/material"
 
 const db = getFirestore(firebaseApp)
 
@@ -21,10 +28,31 @@ export const BetsListArea: React.FC = ({}) => {
   const q = query(
     betsRef,
     where("users", "array-contains", auth.currentUser?.uid ?? ""),
+    orderBy("createdAt", "desc"),
     limit(10),
   ) as Query<Bet>
 
   const [bets, isLoading] = useCollectionData<Bet>(q, { idField: "id" }) ?? []
+  const [oldBets, setOldBets] = useState<Bet[]>([])
+
+  const fullBets = [...(bets as Bet[]), ...oldBets]
+
+  const [hasMore, setHasMore] = useState(true)
+
+  const loadMoreBets = async () => {
+    const lastBet = fullBets[fullBets.length - 1]
+    const lastBetId = lastBet.id
+    const q = query(
+      betsRef,
+      where("users", "array-contains", auth.currentUser?.uid ?? ""),
+      limit(10),
+      orderBy("createdAt", "desc"),
+      startAfter(lastBetId),
+    ) as Query<Bet>
+    const moreOldBets = (await getDocs(q)).docs.map((d) => d.data()) as Bet[]
+    setOldBets([...oldBets, ...moreOldBets])
+    if (moreOldBets.length < 10) setHasMore(false)
+  }
 
   return (
     <>
@@ -33,18 +61,33 @@ export const BetsListArea: React.FC = ({}) => {
           className="scrollbar-dropdown ml-0.5 h-72 w-full overflow-y-auto overflow-x-hidden"
           style={{ direction: "rtl" }}
         >
-          <div style={{ direction: "ltr" }} id="bets-list">
-            {bets
-              ?.sort(
-                (a, b) => b.createdAt.nanoseconds - a.createdAt.nanoseconds,
-              )
-              .filter(
-                (bet) => bet.status === "approved" || bet.status === "funded",
-              )
-              .map((bet: Bet, index: number) => (
-                <BetsListItem key={bet.id + index} {...bet} bet={bet} />
-              ))}
-          </div>
+          <InfiniteScroll
+            scrollThreshold="200px"
+            scrollableTarget="scroll-div"
+            dataLength={fullBets?.length ?? 0}
+            next={loadMoreBets}
+            hasMore={hasMore}
+            loader={
+              
+                <LinearProgress />
+              
+            }
+            inverse={true}
+            className="flex flex-col-reverse"
+          >
+            <div style={{ direction: "ltr" }} id="bets-list">
+              {bets
+                ?.sort(
+                  (a, b) => b.createdAt.nanoseconds - a.createdAt.nanoseconds,
+                )
+                .filter(
+                  (bet) => bet.status === "approved" || bet.status === "funded",
+                )
+                .map((bet: Bet, index: number) => (
+                  <BetsListItem key={bet.id + index} {...bet} bet={bet} />
+                ))}
+            </div>
+          </InfiniteScroll>
         </div>
       ) : (
         <div className="mt-10 flex h-72 w-full justify-center text-stone-400 dark:text-stone-400">
