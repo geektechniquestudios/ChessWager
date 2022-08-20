b438618a34ad08dc018d74aa7db94e2d821bf457
geektechniquestudios <geektechniquestudios@gmail.com>
2022-08-20 13:08:07 -0400

    outlined moderator actions dep bump

---
+++ cypress/e2e/moderator_actions.spec.tsx
@@ -0,0 +1,32 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+
+describe("moderators", () => {
+  before(cy.login) // login as admin
+  beforeEach(() => {
+    cy.visit("/")
+  })
+  after(cy.logout)
+
+  it("should be able to ban other users", () => {})
+
+  it("should not be able to be banned by other moderators", () => {})
+
+})
+
+describe("super moderators", () => {
+  before(cy.login) // normal geektechnique user login is a super moderator
+  beforeEach(() => {
+    cy.visit("/")
+  })
+  after(cy.logout)
+
+  it("should be able to ban other users", () => {})
+
+  it("should be able to unban other users", () => {})
+
+  it("should be able to ban other moderators", () => {})
+
+  it("should be able to unban other moderators", () => {})
+
+})
