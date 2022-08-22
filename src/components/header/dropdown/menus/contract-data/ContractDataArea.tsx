15e2c28e76338c33464a6bcb9f4aba0e74c69b9c
geektechniquestudios <geektechniquestudios@gmail.com>
2022-08-21 22:19:28 -0400

    -bugfix to 'p can't be child of p' -isCreator for contract interaction is now dynamic

--- src/components/header/dropdown/menus/contract-data/ContractDataArea.tsx
+++ src/components/header/dropdown/menus/contract-data/ContractDataArea.tsx
@@ -100,14 +100,14 @@ export const ContractDataArea: React.FC<Props> = ({}) => {
     <div className="flex h-60 w-full justify-center">
       <div className="flex flex-col justify-evenly">
         <div className="flex flex-col gap-2">
-          <p className="flex justify-center text-3xl">
+          <div className="flex justify-center text-3xl">
             <p className="p-0.5 text-sm">$</p>
             {contractBalanceUSD.toFixed(2)}
-          </p>
-          <p className="flex justify-center text-3xl">
+          </div>
+          <div className="flex justify-center text-3xl">
             {contractBalanceAVAX.toFixed(6)}
             <p className="flex flex-col-reverse p-0.5 text-sm">AVAX</p>
-          </p>
+          </div>
         </div>
         <button
           className="color-shift clickable rounded-md border border-stone-500 bg-stone-200 px-2 py-1.5 font-bold text-stone-800 hover:border-black hover:bg-white hover:text-stone-800 dark:border-stone-500 dark:bg-stone-900 dark:text-stone-300 dark:hover:border-stone-300 dark:hover:bg-stone-800 dark:hover:text-stone-300"
--- src/components/header/dropdown/menus/persona/buttons/ContractDataButton.tsx
+++ src/components/header/dropdown/menus/persona/buttons/ContractDataButton.tsx
@@ -1,14 +1,15 @@
-import { RiBankLine, RiPenNibLine, RiSkull2Line } from "react-icons/ri"
+import { RiPenNibLine } from "react-icons/ri"
 import { DropdownState } from "../../../../../containers/DropdownState"
 import { UserDataState } from "../../../../../containers/UserDataState"
 import { DropdownButton } from "./DropdownButton"
+
+const adminWallet = import.meta.env.VITE_METAMASK_ACCOUNT_ADDRESS
 
 interface Props {}
 
 export const ContractDataButton: React.FC<Props> = ({}) => {
   const { userData } = UserDataState.useContainer()
-  const isCreator =
-    userData?.walletAddress === "0x9497DA534Dcb2B49d73dEf1468b91339e98BE60C"
+  const isCreator = (userData?.walletAddress ?? 1) === (adminWallet ?? 2)
   const { setActiveMenu, menuStack, setMenuStack } =
     DropdownState.useContainer()
   return (
