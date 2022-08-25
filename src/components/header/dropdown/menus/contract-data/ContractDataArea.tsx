e2927d60f5f6d30d5ec9f3dba61d2fdb66c8c94c
geektechniquestudios <geektechniquestudios@gmail.com>
2022-08-24 21:06:09 -0400

    Contract withdrawals are functional. Docker files temporarily modded for pi-hack branch

--- src/components/header/dropdown/menus/contract-data/ContractDataArea.tsx
+++ src/components/header/dropdown/menus/contract-data/ContractDataArea.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { ethers } from "ethers"
+import { ethers, Transaction } from "ethers"
 import ChessWager from "../../../../../artifacts/contracts/ChessWager.sol/ChessWager.json"
 import { Price } from "../../../../containers/Price"
 
@@ -89,25 +89,28 @@ export const ContractDataArea: React.FC<Props> = ({}) => {
   }
 
   const withdrawBalance = async (contract: ethers.Contract) => {
-    return await contract.withdrawChessWagerBalance()
+    await contract.withdrawChessWagerBalance()
   }
 
   useEffect(() => {
-    callContract(getBalance)
+    const interval = setInterval(() => {
+      callContract(getBalance)
+    }, 3000)
+    return () => clearInterval(interval)
   }, [])
 
   return (
     <div className="flex h-60 w-full justify-center">
       <div className="flex flex-col justify-evenly">
         <div className="flex flex-col gap-2">
-          <p className="flex justify-center text-3xl">
+          <div className="flex text-3xl">
             <p className="p-0.5 text-sm">$</p>
             {contractBalanceUSD.toFixed(2)}
-          </p>
-          <p className="flex justify-center text-3xl">
+          </div>
+          <div className="flex text-3xl">
             {contractBalanceAVAX.toFixed(6)}
             <p className="flex flex-col-reverse p-0.5 text-sm">AVAX</p>
-          </p>
+          </div>
         </div>
         <button
           className="color-shift clickable rounded-md border border-stone-500 bg-stone-200 px-2 py-1.5 font-bold text-stone-800 hover:border-black hover:bg-white hover:text-stone-800 dark:border-stone-500 dark:bg-stone-900 dark:text-stone-300 dark:hover:border-stone-300 dark:hover:bg-stone-800 dark:hover:text-stone-300"
