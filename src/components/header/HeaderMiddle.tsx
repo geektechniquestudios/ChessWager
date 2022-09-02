281f0413567df6ffea5b2859b6de0b2eeefcc2f3
geektechniquestudios <geektechniquestudios@gmail.com>
2022-09-01 22:10:12 -0400

    onboarding improved

--- src/components/header/HeaderMiddle.tsx
+++ src/components/header/HeaderMiddle.tsx
@@ -1,15 +1,25 @@
 import "../../style/buttons.scss"
 import { Auth } from "../containers/Auth"
+import { DropdownState } from "../containers/DropdownState"
 
 interface Props {}
 
 export const HeaderMiddle: React.FC<Props> = ({}) => {
   const { isFirstLogin } = Auth.useContainer()
+  const { menuStack, setMenuStack, openDropdownToMenu } =
+    DropdownState.useContainer()
+
   return (
     <div className="flex-auto" id="header-middle">
       <div className="flex justify-center">
         {isFirstLogin && (
-          <button className="color-shift clickable animate-pulse rounded-md border border-stone-500 bg-stone-200 px-2 py-1.5 font-bold text-stone-800 hover:border-black hover:bg-white hover:text-stone-800 dark:border-stone-500 dark:bg-stone-900 dark:text-stone-300 dark:hover:border-stone-300 dark:hover:bg-stone-800 dark:hover:text-stone-300">
+          <button
+            onClick={() => {
+              setMenuStack([...menuStack, "howToPlay"])
+              openDropdownToMenu("howToPlay")
+            }}
+            className="color-shift clickable animate-pulse rounded-md border border-stone-500 bg-stone-200 px-2 py-1.5 font-bold text-stone-800 hover:border-black hover:bg-white hover:text-stone-800 dark:border-stone-500 dark:bg-stone-900 dark:text-stone-300 dark:hover:border-stone-300 dark:hover:bg-stone-800 dark:hover:text-stone-300"
+          >
             Make your first bet
           </button>
         )}
--- src/components/header/dropdown/menus/help/HowToPlay.tsx
+++ src/components/header/dropdown/menus/help/HowToPlay.tsx
@@ -1,8 +1,55 @@
+import { ethers } from "ethers"
+import { useEffect, useState } from "react"
 import { DropdownArea } from "../../models/DropdownArea"
 import { Menu } from "../../models/Menu"
 import { SectionWrapper } from "./SectionWrapper"
 
+declare let window: any
+
 export const HowToPlay: React.FC = ({}) => {
+  const isMetamaskInstalled = typeof window.ethereum !== undefined
+  // use this version for mainnet inclusion
+  // const isCorrectBlockchain = async (
+  //   provider: ethers.providers.Web3Provider,
+  // ) => {
+  //   const { chainId } = await provider.getNetwork()
+  //   if (isLocal && chainId !== 43113) {
+  //     alert("You are on the wrong network. Please switch to the fuji network.")
+  //     return false
+  //   }
+  //   else if (!isLocal && chainId !== 43114) {
+  //     alert(
+  //       "You are on the wrong network. Please switch to the avalanche mainnet.",
+  //     )
+  //     return false
+  //   }
+  //   else {
+  //     return true
+  //   }
+  // }
+
+  //
+  // use this version until mainnet
+
+  const checkNetowrk = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" })
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const { chainId } = await provider.getNetwork()
+    return chainId === 43113
+  }
+
+  const [isCorrectNetwork, setIsCorrectNetwork] = useState(false)
+
+  useEffect(() => {
+    checkNetowrk().then(setIsCorrectNetwork)
+  })
+
+  const isWalletBalancePositive = () => {
+    return true
+  }
+
+  const connectWallet = async () => {}
+
   return (
     <Menu
       menuItems={[
@@ -12,14 +59,24 @@ export const HowToPlay: React.FC = ({}) => {
               <SectionWrapper
                 text="1. Add the Metamask extension to your browser"
                 href="https://metamask.io/download/"
+                isComplete={isMetamaskInstalled}
               />
               <SectionWrapper
                 text="2. Add the Avalanche network to Metamask"
                 href="https://chainlist.org/chain/43114/"
+                isComplete={isCorrectNetwork}
               />
               <SectionWrapper
                 text="3. Fund your wallet"
                 href="https://app.pangolin.exchange/#/buy"
+                isComplete={isWalletBalancePositive()}
+              />
+              <SectionWrapper
+                text="4. Connect your wallet"
+                onClick={() => {
+                  connectWallet()
+                }}
+                isComplete={true}
               />
             </div>
           }
