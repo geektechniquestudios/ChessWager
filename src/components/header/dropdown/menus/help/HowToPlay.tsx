3e7193a433cbf146ea20152ec4c16f5c00b32491
geektechniquestudios <geektechniquestudios@gmail.com>
2022-09-02 21:20:23 -0400

    another bandaid

--- src/components/header/dropdown/menus/help/HowToPlay.tsx
+++ src/components/header/dropdown/menus/help/HowToPlay.tsx
@@ -7,7 +7,6 @@ import { SectionWrapper } from "./SectionWrapper"
 declare let window: any
 
 export const HowToPlay: React.FC = ({}) => {
-  const isMetamaskInstalled = typeof window.ethereum !== undefined
   // use this version for mainnet inclusion
   // const isCorrectBlockchain = async (
   //   provider: ethers.providers.Web3Provider,
@@ -30,26 +29,7 @@ export const HowToPlay: React.FC = ({}) => {
 
   //
   // use this version until mainnet
-
-  const checkNetowrk = async () => {
-    // await window.ethereum.request({ method: "eth_requestAccounts" })
-    // const provider = new ethers.providers.Web3Provider(window.ethereum)
-    // const { chainId } = await provider.getNetwork()
-    // return chainId === 43113
-    return true
-  }
-
-  const [isCorrectNetwork, setIsCorrectNetwork] = useState(false)
-
-  useEffect(() => {
-    checkNetowrk().then(setIsCorrectNetwork)
-  }, [])
-
-  const isWalletBalancePositive = () => {
-    return true
-  }
-
-  const connectWallet = async () => {}
+  
 
   return (
     <Menu
@@ -60,25 +40,14 @@ export const HowToPlay: React.FC = ({}) => {
               <SectionWrapper
                 text="1. Add the Metamask extension to your browser"
                 href="https://metamask.io/download/"
-                isComplete={isMetamaskInstalled}
               />
               <SectionWrapper
                 text="2. Add the Avalanche network to Metamask"
                 href="https://chainlist.org/chain/43114/"
-                isComplete={isCorrectNetwork}
               />
               <SectionWrapper
                 text="3. Fund your wallet"
                 href="https://app.pangolin.exchange/#/buy"
-                isComplete={isWalletBalancePositive()}
-                // isComplete={true}
-              /> 
-               <SectionWrapper
-                text="4. Connect your wallet"
-                onClick={() => {
-                  // connectWallet()
-                }}
-                isComplete={true}
               />
             </div>
           }
--- src/components/header/dropdown/menus/help/SectionWrapper.tsx
+++ src/components/header/dropdown/menus/help/SectionWrapper.tsx
@@ -1,23 +1,18 @@
 interface Props {
   text: string
   href?: string
-  isComplete: boolean
   onClick?: () => void
 }
 
 export const SectionWrapper: React.FC<Props> = ({
   text,
   href,
-  isComplete,
   onClick,
 }) => {
-  const isCompleteStyle = isComplete
-    ? "border-green-600"
-    : "border-stone-600 dark:border-stone-500"
   return (
     <a
       href={href}
-      className={`color-shift rounded-md border bg-stone-100 p-2 hover:text-stone-900 dark:bg-stone-800 dark:hover:border-white dark:hover:text-stone-200 ${isCompleteStyle}`}
+      className="color-shift rounded-md border bg-stone-100 p-2 hover:text-stone-900 dark:bg-stone-800 dark:hover:border-white dark:hover:text-stone-200"
       target="_blank"
       rel="noopener noreferrer"
       onClick={onClick}
