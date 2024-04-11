48feaf5d34bd3df397a9e7465af3456f319a8d80
spaceC00kie <kirstie317@gmail.com>
2024-04-11 16:54:12 -0400

    update how to play menu colors add hover colors to friend request item adjust unblock button color

--- src/components/header/dropdown/menus/help/SectionWrapper.tsx
+++ src/components/header/dropdown/menus/help/SectionWrapper.tsx
@@ -16,11 +16,11 @@ export const SectionWrapper: React.FC<Props> = ({
   isComplete,
 }) => {
   return (
-    <div className="color-shift relative flex justify-between gap-2 rounded-md border border-stone-300 bg-stone-100 p-2 dark:border-stone-500 dark:bg-stone-600">
+    <div className="color-shift relative flex justify-between gap-2 rounded-md border border-stone-400 bg-stone-100 p-2 dark:border-stone-500 dark:bg-stone-600">
       <p className="grid place-content-center">{text}</p>
       <div className="grid place-content-center">
         <a
-          className="color-shift grid h-9 w-9 shrink-0 place-content-center rounded-md border dark:border-stone-400 dark:hover:border-stone-500 dark:hover:bg-stone-500"
+          className="color-shift grid h-9 w-9 shrink-0 place-content-center rounded-md border border-stone-300 hover:bg-white hover:border-stone-400 dark:border-stone-400 dark:hover:border-stone-500 dark:hover:bg-stone-500"
           href={href}
           target="_blank"
           rel="noopener noreferrer"
--- src/components/header/dropdown/menus/requests/RequestItem.tsx
+++ src/components/header/dropdown/menus/requests/RequestItem.tsx
@@ -82,7 +82,7 @@ export const RequestItem: React.FC<Props> = ({
           }}
           content={<BsCheck2 />}
           title="Accept"
-          className="h-4 w-4"
+          className="h-4 w-4 hover:bg-teal-100 dark:hover:bg-green-900"
         />
         <DropdownButton
           onClick={(e) => {
@@ -91,7 +91,7 @@ export const RequestItem: React.FC<Props> = ({
           }}
           content={<BsX />}
           title="Decline"
-          className="h-4 w-4"
+          className="h-4 w-4 hover:bg-stone-400 dark:hover:bg-black"
         />
       </div>
     </a>
