b939ea6dd3e1271d787f5bd8025e64f2a590964f
geektechniquestudios <geektechniquestudios@gmail.com>
2025-01-15 02:45:29 -0500

    Orb animates to show bet state update to style on warning popup

--- src/components/lobby/bet/right/user-data/User2Image.tsx
+++ src/components/lobby/bet/right/user-data/User2Image.tsx
@@ -1,10 +1,11 @@
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { Bet } from "../../../../../interfaces/Bet"
 import { AuthState } from "../../../../../containers/AuthState"
 import { DropdownState } from "../../../../../containers/DropdownState"
 import { UserMenuState } from "../../../../../containers/UserMenuState"
 import { User2BetAmount } from "./User2BetAmount"
 import { User2FollowThrough } from "./User2FollowThrough"
+import { ActivityOrb } from "./ActivityOrb"
 
 interface Props {
   bet: Bet
@@ -16,13 +17,22 @@ export const User2Image: React.FC<Props> = ({ bet }) => {
   const { openDropdownToMenu } = DropdownState.useContainer()
   const { setClickedUserById } = UserMenuState.useContainer()
   const { user } = AuthState.useContainer()
+
   const disabledStyle = !user ? "pointer-events-none" : ""
   const ringStyle = betSide === "black" ? "white-ring" : "black-ring"
   const ringBorderStyle =
     betSide === "black" ? "white-ring-border" : "black-ring-border"
 
+  const isUserThinking = false
+
   return (
     <div className={`${ringBorderStyle} relative shrink-0 rounded-full border`}>
+      <ActivityOrb
+        bet={bet}
+        isUserThinking={isUserThinking}
+        hasUserPaid={bet.hasUser2Paid}
+      />
+
       {user2PhotoURL && (
         <motion.a
           initial={{ rotate: 90 }}
@@ -43,13 +53,42 @@ export const User2Image: React.FC<Props> = ({ bet }) => {
       )}
 
       {!user2PhotoURL && (
-        <motion.div
+        <div
           className={`${disabledStyle} ${ringStyle} flex h-full w-full rounded-full border-4 font-extrabold`}
         >
           <div
             className={`${ringBorderStyle} ring-center h-8 w-8 shrink-0 rounded-full border`}
-          />
-        </motion.div>
+          ></div>
+        </div>
+      )}
+
+      {!user2PhotoURL && (
+        <div className="color-shift pointer-events-none absolute inset-0">
+          <AnimatePresence>
+            {bet.status === "ready" && (
+              <motion.div
+                className="absolute inset-0 z-50 m-auto h-3 w-3"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                }}
+                exit={{ opacity: 0 }}
+              >
+                <div className="relative flex h-3 w-3 blur-[1px]">
+                  <div
+                    className={`absolute inline-flex h-full w-full animate-[ping_2s_ease-in-out_infinite] rounded-full bg-sky-500`}
+                  />
+                  <div
+                    className={`inline-flex h-3 w-3 rounded-full bg-sky-500`}
+                  />
+                  <div className="absolute inset-0 m-auto h-1 w-1 animate-pulse bg-white blur-[1px]" />
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       )}
 
       <User2BetAmount bet={bet} />
